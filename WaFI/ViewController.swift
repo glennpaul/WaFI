f1d2655273aa04ea2af5acb4806f927cd39a0670
Paul Sumido <paulsumido@pauls-macbook-pro.local>
2018-06-03 21:03:00 -0400

    Initial Commit

---
+++ WaFI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaFI
+//
+//  Created by Paul Sumido on 2018-06-03.
+//  Copyright © 2018 Paul Sumido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
