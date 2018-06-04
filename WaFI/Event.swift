0f2ea72edf2a65b7da7c9599c2822229b08f8b70
Paul Sumido <paulsumido@pauls-macbook-pro.local>
2018-06-04 08:50:03 -0400

    Created event data model and table cell class.

---
+++ WaFI/Event.swift
@@ -0,0 +1,33 @@
+//
+//  Event.swift
+//  WaFI
+//
+//  Created by Paul Sumido on 2018-06-04.
+//  Copyright © 2018 Paul Sumido. All rights reserved.
+//
+
+import UIKit
+
+class Event {
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var date: Date
+    
+    
+    init?(name: String, photo: UIImage?, date: Date) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || date <= Date()  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.date = date
+        
+    }
+    
+}
---
+++ WaFI/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  WaFI
+//
+//  Created by Paul Sumido on 2018-06-04.
+//  Copyright © 2018 Paul Sumido. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
