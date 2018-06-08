e999ff3ca0cc17cd3a14c403655bd46a2d4cf4ee
Paul Sumido <paulsumido@pauls-macbook-pro.local>
2018-06-07 22:16:45 -0400

    added project to firebase

---
+++ Pods/Target Support Files/Pods-WaFI/Pods-WaFI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WaFI : NSObject
+@end
+@implementation PodsDummy_Pods_WaFI
+@end
---
+++ Pods/Target Support Files/Pods-WaFITests/Pods-WaFITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WaFITests : NSObject
+@end
+@implementation PodsDummy_Pods_WaFITests
+@end
